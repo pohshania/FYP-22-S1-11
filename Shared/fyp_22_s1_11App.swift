f70c67b29e3c49e4ae170df730d6ca6731b3c831
Minjung22 <73127398+Minjung22@users.noreply.github.com>
2022-02-15 15:04:39 +0800

    added Xcode files

---
+++ Shared/fyp_22_s1_11App.swift
@@ -0,0 +1,17 @@
+//
+//  fyp_22_s1_11App.swift
+//  Shared
+//
+//  Created by Minjung Ghang on 15/2/22.
+//
+
+import SwiftUI
+
+@main
+struct fyp_22_s1_11App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

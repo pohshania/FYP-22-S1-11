6bfeeb66f89f5e05bd22a19c9881a34636fb62ea
pohshania <pohshania@gmail.com>
2022-02-16 15:04:48 +0800

    test

---
+++ Andriod/FYP22S111IDS/app/src/main/java/com/example/fyp_22_s1_11ids/ui/home/HomeViewModel.java
@@ -0,0 +1,19 @@
+package com.example.fyp_22_s1_11ids.ui.home;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class HomeViewModel extends ViewModel {
+
+    private final MutableLiveData<String> mText;
+
+    public HomeViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is home fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}

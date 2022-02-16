52b7cfafa18533ef7a515d16676ab2036f1a8cad
LeeYT0403 <leeyt1998@gmail.com>
2022-02-16 20:43:59 +0800

    Buttons work now
    Added codes to the buttons, login brings u to a Homepage(to be coded)
    Sign up goes to sign up page
    Sign up in sign up page shows a message that says registered successfully

--- Andriod/FYPApp/app/src/main/java/com/uowmail/fypapp/MainActivity.java
+++ Andriod/FYPApp/app/src/main/java/com/uowmail/fypapp/MainActivity.java
@@ -2,7 +2,11 @@ package com.uowmail.fypapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //Declare signup button and login button
+        Button loginButton = (Button) findViewById(R.id.loginButton);
+        Button signupButton = (Button) findViewById(R.id.signupButton);
+
+        //Sets action for sign up button
+        signupButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this,SignUpActivity.class));
+            }
+        });
+        //Sets action for login button
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this,HomeActivity.class));
+            }
+        });
     }
 }
--- Andriod/FYPApp/app/src/main/java/com/uowmail/fypapp/SignUpActivity.java
+++ Andriod/FYPApp/app/src/main/java/com/uowmail/fypapp/SignUpActivity.java
@@ -3,6 +3,9 @@ package com.uowmail.fypapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class SignUpActivity extends AppCompatActivity {
 
@@ -10,5 +13,15 @@ public class SignUpActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        //declare signup button
+        Button signNowButton = (Button) findViewById(R.id.signNowButton);
+
+        signNowButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v)  {
+                Toast.makeText(getBaseContext(), "Account registered successfully" , Toast.LENGTH_SHORT ).show();
+            }
+        });
+
     }
 }

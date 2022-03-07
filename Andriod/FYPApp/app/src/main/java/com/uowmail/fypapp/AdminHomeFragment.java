801af4c6ad5a5b6b4e737eb6554815ba4c37eab2
Minjung22 <73127398+Minjung22@users.noreply.github.com>
2022-03-07 18:58:05 +0800

    changed bottom nav bar
    Client menu = home, notification, logs
    Admin menu = home, notification, settings
    
    added Logs page for client user

--- Andriod/FYPApp/app/src/main/java/com/uowmail/fypapp/AdminHomeFragment.java
+++ Andriod/FYPApp/app/src/main/java/com/uowmail/fypapp/AdminHomeFragment.java
@@ -3,11 +3,18 @@ package com.uowmail.fypapp;
 import android.os.Bundle;
 import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.material.badge.BadgeDrawable;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
 public class AdminHomeFragment extends Fragment {
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
--- Andriod/FYPApp/app/src/main/java/com/uowmail/fypapp/HomeActivity.java
+++ Andriod/FYPApp/app/src/main/java/com/uowmail/fypapp/HomeActivity.java
@@ -2,27 +2,16 @@ package com.uowmail.fypapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.graphics.Color;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.AdapterView;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.github.mikephil.charting.charts.PieChart;
-import com.github.mikephil.charting.components.Legend;
-import com.github.mikephil.charting.data.PieData;
-import com.github.mikephil.charting.data.PieDataSet;
-import com.github.mikephil.charting.data.PieEntry;
-import com.github.mikephil.charting.formatter.PercentFormatter;
-import com.github.mikephil.charting.utils.ColorTemplate;
 import com.google.android.material.badge.BadgeDrawable;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationBarView;
-
-import java.util.ArrayList;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -32,6 +21,7 @@ public class HomeActivity extends AppCompatActivity {
     AdminHomeFragment adminHomeFragment = new AdminHomeFragment();
     NotificationFragment notificationFragment = new NotificationFragment();
     SettingsFragment settingsFragment = new SettingsFragment();
+    LogsFragment logsFragment = new LogsFragment();
 
     // true -> admin,  false -> client
     private boolean admin;
@@ -41,26 +31,32 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getSupportActionBar().hide();
-        setContentView(R.layout.activity_home);
 
         // MJ - bottom nav bar -------------------------------------------------------------------------------
-        bottomNavigationView = findViewById(R.id.bottom_navigation);
-
         if (getIntent().getSerializableExtra("adminbool").toString().equals("admin"))
             admin = true;
         else
             admin = false;
 
         // replace fragment (id = container) with admin/clientHomeFragment
-        if (admin == true)
+        if (admin)
+        {
+          setContentView(R.layout.activity_home_admin);
             getSupportFragmentManager().beginTransaction().replace(R.id.container, adminHomeFragment).commit();
+            bottomNavigationView = findViewById(R.id.bottom_navigation_admin);
+        }
         else
+        {
+            setContentView(R.layout.activity_home_client);
             getSupportFragmentManager().beginTransaction().replace(R.id.container, clientHomeFragment).commit();
+            bottomNavigationView = findViewById(R.id.bottom_navigation_client);
+        }
 
         // badge notification alert
         BadgeDrawable badgeDrawable = bottomNavigationView.getOrCreateBadge(R.id.notification);
         badgeDrawable.setVisible(true);
         badgeDrawable.setNumber(8);
+
 
         bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
             @Override
@@ -80,6 +76,10 @@ public class HomeActivity extends AppCompatActivity {
 
                     case R.id.settings:
                         getSupportFragmentManager().beginTransaction().replace(R.id.container, settingsFragment).commit();
+                        return true;
+
+                    case R.id.logs:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container, logsFragment).commit();
                         return true;
                 }
                 return false;

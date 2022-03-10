1df4a46f7f043ea0b43bdf3b24f14faf6e273672
Minjung22 <73127398+Minjung22@users.noreply.github.com>
2022-03-11 02:13:49 +0800

    add rules page for Admin
    pwdpop.xml is popup window when 'save change' button is clicked after changing rules

--- Andriod/FYPApp/app/src/main/java/com/uowmail/fypapp/AdminHomeFragment.java
+++ Andriod/FYPApp/app/src/main/java/com/uowmail/fypapp/AdminHomeFragment.java
@@ -1,11 +1,19 @@
 package com.uowmail.fypapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
 import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -15,10 +23,25 @@ import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationBarView;
 
 public class AdminHomeFragment extends Fragment {
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_admin_home, container, false);
+        View v = inflater.inflate(R.layout.fragment_admin_home, container, false);
+
+        // MJ - Buttons for admins
+        Button rulesButton = v.findViewById(R.id.buttonRules);
+
+        // MJ - open RulesActivity when button is clicked
+        rulesButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), RulesActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        return v;
     }
+
 }
---
+++ Andriod/FYPApp/app/src/main/java/com/uowmail/fypapp/RulesActivity.java
@@ -0,0 +1,121 @@
+package com.uowmail.fypapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.AlertDialog;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.LinearLayout;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+public class RulesActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+    private Spinner spinner;
+
+    // MJ - Pop up window to enter pwd --------------------------------------------------------------------------------------
+    private AlertDialog.Builder dialogBuilder;
+    private AlertDialog dialog;
+    private Button saveChange,cancelBtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rules);
+        getSupportActionBar().hide();
+
+        // MJ - Dropdown box for System type --------------------------------------------------------------------------------------
+        spinner = findViewById(R.id.spinner_systemType);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,R.array.systemType, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(this);
+
+        // MJ - btn to save rules
+        Button submitBtn = (Button) findViewById(R.id.submitButton);
+        submitBtn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                // define save button here!
+                createNewContactDialog();
+//                Toast.makeText( RulesActivity.this, "You clicked save change button", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+
+    @Override
+    // MJ - Dropdown box for System type --------------------------------------------------------------------------------------
+    // MJ - List down the rules according to system type-----------------------------------------------------------------------
+    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+        String choice = adapterView.getItemAtPosition(i).toString();
+
+        LinearLayout linearLayout = (LinearLayout) findViewById(R.id.ruleContainer);
+
+        // show list of rules with check box
+        switch (choice){
+            case "email system":
+                // remove all the views first
+                linearLayout.removeAllViews();
+                // save rules of email system into array
+                String[] ruleArray = getResources().getStringArray(R.array.rules_email);
+
+                for (int count = 0; count < ruleArray.length; count++) {
+                    // create checkboxes for rules
+                    CheckBox checkBox = new CheckBox(this);
+                    checkBox.setText(ruleArray[count]);
+                    checkBox.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT));
+
+                    linearLayout.addView(checkBox);
+                }
+                break;
+
+            case "System Type 1":
+                // remove all the views first
+                linearLayout.removeAllViews();
+                // save rules of email system into array
+                ruleArray = getResources().getStringArray(R.array.rules_1);
+
+                for (int count = 0; count < ruleArray.length; count++) {
+                    // create checkboxes for rules
+                    CheckBox checkBox = new CheckBox(this);
+                    checkBox.setText(ruleArray[count]);
+                    checkBox.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT));
+
+                    linearLayout.addView(checkBox);
+                }
+                break;
+        }
+
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
+
+    }
+
+    // MJ - Popup window to enter password --------------------------------------------------------------------------------------
+    public void createNewContactDialog(){
+        dialogBuilder = new AlertDialog.Builder(this);
+        final View passwordPopupView = getLayoutInflater().inflate(R.layout.pwdpopup, null);
+
+        saveChange = (Button) passwordPopupView.findViewById(R.id.saveButton);
+        cancelBtn = (Button) passwordPopupView.findViewById(R.id.cancelButton);
+
+        dialogBuilder.setView(passwordPopupView);
+        dialog = dialogBuilder.create();
+        dialog.show();
+
+        saveChange.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                // define save button here!
+                dialog.dismiss();
+            }
+        });
+
+    }
+}

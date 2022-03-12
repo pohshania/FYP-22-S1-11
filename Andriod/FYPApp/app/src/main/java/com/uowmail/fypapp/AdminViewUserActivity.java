b10eacf19660d90d716b58604e01625854e47a46
Jessica Hardisurjo <jessicahardisurjo88@gmail.com>
2022-03-12 17:26:24 +0800

    Rename Fragment
    Renamed a java class AdminViewUserFragment to AdminViewUserActivity

--- Andriod/FYPApp/app/src/main/java/com/uowmail/fypapp/AdminViewUserActivity.java
+++ Andriod/FYPApp/app/src/main/java/com/uowmail/fypapp/AdminViewUserActivity.java
@@ -7,19 +7,19 @@ import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class AdminViewUserFragment extends AppCompatActivity {
+public class AdminViewUserActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getSupportActionBar().hide();
         setContentView(R.layout.activity_sign_up);
-        TextView userName = (TextView) findViewById(R.id.Username1);
+        TextView userName = (TextView) findViewById(R.id.Username2);
 
         //Sets action for sign up button
         userName.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                startActivity(new Intent(AdminViewUserFragment.this, LogsFragment.class));
+                startActivity(new Intent(AdminViewUserActivity.this, LogsFragment.class));
             }
         });
     }

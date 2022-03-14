ee4084490eb230257f7949c5ed72d57a5962f271
Minjung22 <73127398+Minjung22@users.noreply.github.com>
2022-03-14 22:28:24 +0800

    Change header for notification page

--- Andriod/FYPApp/app/src/main/java/com/uowmail/fypapp/NotificationFragment.java
+++ Andriod/FYPApp/app/src/main/java/com/uowmail/fypapp/NotificationFragment.java
@@ -7,13 +7,21 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 public class NotificationFragment extends Fragment {
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_notification, container, false);
+         View v = inflater.inflate(R.layout.fragment_notification, container, false);
+
+        TextView title = (TextView)getActivity().findViewById(R.id.toolbar_title);
+        title.setText("Notification");
+
+
+        return v;
     }
 }

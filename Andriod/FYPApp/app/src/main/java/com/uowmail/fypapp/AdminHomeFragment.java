5ecfc04649fb516e7d621d36098e444c58677488
pohshania <pohshania@gmail.com>
2022-03-14 22:14:39 +0800

    action bars

--- Andriod/FYPApp/app/src/main/java/com/uowmail/fypapp/AdminHomeFragment.java
+++ Andriod/FYPApp/app/src/main/java/com/uowmail/fypapp/AdminHomeFragment.java
@@ -16,6 +16,9 @@ public class AdminHomeFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+
+
         // Inflate the layout for this fragment
         View v = inflater.inflate(R.layout.fragment_admin_home, container, false);
 
--- Andriod/FYPApp/app/src/main/java/com/uowmail/fypapp/AdminViewUserLogsActivity.java
+++ Andriod/FYPApp/app/src/main/java/com/uowmail/fypapp/AdminViewUserLogsActivity.java
@@ -1,5 +1,6 @@
 package com.uowmail.fypapp;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
@@ -10,5 +11,8 @@ public class AdminViewUserLogsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_view_user_logs);
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setTitle("User Logs");
     }
 }

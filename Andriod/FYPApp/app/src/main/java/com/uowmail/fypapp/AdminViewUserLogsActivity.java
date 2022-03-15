b81563830a19027ad44c4ab9424265da9799e9d3
Minjung22 <73127398+Minjung22@users.noreply.github.com>
2022-03-15 14:27:05 +0800

    Add popup window for deleting logs
    When deleting logs, popup window will alert user to enter password

--- Andriod/FYPApp/app/src/main/java/com/uowmail/fypapp/AdminViewUserLogsActivity.java
+++ Andriod/FYPApp/app/src/main/java/com/uowmail/fypapp/AdminViewUserLogsActivity.java
@@ -3,9 +3,17 @@ package com.uowmail.fypapp;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class AdminViewUserLogsActivity extends AppCompatActivity {
+
+    // MJ - Pop up window to enter pwd --------------------------------------------------------------------------------------
+    private AlertDialog.Builder dialogBuilder;
+    private AlertDialog dialog;
+    private Button saveChange, cancelBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +24,37 @@ public class AdminViewUserLogsActivity extends AppCompatActivity {
         ActionBar actionBar = getSupportActionBar();
         actionBar.setTitle("User Logs");
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        // MJ - btn to save rules
+        Button deleteBtn = (Button) findViewById(R.id.deleteButton);
+        deleteBtn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                // define save button here!
+                createNewContactDialog();
+//                Toast.makeText( RulesActivity.this, "You clicked save change button", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+    // MJ - Popup window to enter password --------------------------------------------------------------------------------------
+    public void createNewContactDialog(){
+        dialogBuilder = new AlertDialog.Builder(this);
+        final View passwordPopupView = getLayoutInflater().inflate(R.layout.delete_logs_popup, null);
+
+        saveChange = (Button) passwordPopupView.findViewById(R.id.saveButton);
+        cancelBtn = (Button) passwordPopupView.findViewById(R.id.cancelButton);
+
+        dialogBuilder.setView(passwordPopupView);
+        dialog = dialogBuilder.create();
+        dialog.show();
+
+        saveChange.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                // define save button here!
+                dialog.dismiss();
+            }
+        });
 
     }
 

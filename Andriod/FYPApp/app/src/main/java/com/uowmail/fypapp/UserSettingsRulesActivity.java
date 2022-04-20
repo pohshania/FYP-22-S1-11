9888d1eb4f3ebec2863d183933fb9b45192993b3
Minjung22 <73127398+Minjung22@users.noreply.github.com>
2022-04-20 21:24:47 +0800

    userSettingsRule page done

--- Andriod/FYPApp/app/src/main/java/com/uowmail/fypapp/UserSettingsRulesActivity.java
+++ Andriod/FYPApp/app/src/main/java/com/uowmail/fypapp/UserSettingsRulesActivity.java
@@ -3,13 +3,20 @@ package com.uowmail.fypapp;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
+import android.content.res.ColorStateList;
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
+import android.graphics.drawable.Icon;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.CheckBox;
 import android.widget.LinearLayout;
 import android.widget.Spinner;
 import android.widget.TextView;
@@ -29,10 +36,11 @@ public class UserSettingsRulesActivity extends AppCompatActivity {
 
     // read rules from the firebase and print on this page
     FirebaseFirestore db;
-    String oneLine;
-    String cpu, net_send, net_recv, disk_read, disk_write;
-    private TextView tv_cpu, tv_nets, tv_netr, tv_diskr, tv_diskw, tv_net, tv_disk;
+    String rule;
+    String cpu_max, net_send, net_recv, disk_read, disk_write, cpu, netR, netS, diskR, diskW;
     // todo - trying to add org id dynamically
+    LinearLayout cpu_ll, net_ll, disk_ll;
+
 
     private String orgID;
 
@@ -56,16 +64,8 @@ public class UserSettingsRulesActivity extends AppCompatActivity {
             Log.d("====CURRENT ADMIN'S ORG ID====", orgID);
         }
 
-
-
-        tv_cpu       = findViewById(R.id.rule_cpu);
-        tv_net       = findViewById(R.id.rule_network);
-        tv_disk      = findViewById(R.id.rule_disk);
-//        tv_nets      = findViewById(R.id.rule_network);
-//        tv_netr      = findViewById(R.id.rule_network);
-//        tv_diskr     = findViewById(R.id.rule_disk);
-//        tv_diskw     = findViewById(R.id.rule_disk);
-// shania
+        // get rules from the database display them.
+        getRulesList();
 
 
 //        //Getting the instance of Spinner and applying OnItemSelectedListener on it
@@ -85,20 +85,10 @@ public class UserSettingsRulesActivity extends AppCompatActivity {
 //    }
 //    @Override
 //    public void onNothingSelected(AdapterView<?> arg0) {
-//        // TODO Auto-generated method stub
 //    }
-//    // MJ - enable back button in ActionBar
-//    @Override
-//    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-//        switch (item.getItemId()) {
-//            case android.R.id.home:
-//                this.finish();
-//                return true;
-//        }
-//        return super.onOptionsItemSelected(item);
     }
 
-    public void getRulesList(CurrentUserInfo currentUserInfo)
+    public void getRulesList()
     {
 //        String path = currentUserInfo.getOrgID() + "_detection";
         String path = orgID + "_detection";
@@ -110,8 +100,8 @@ public class UserSettingsRulesActivity extends AppCompatActivity {
                 if (task.isSuccessful()) {
                     DocumentSnapshot doc = task.getResult();
                     if (doc.exists()) {
-                        oneLine= String.valueOf(doc.getData());
-                        Log.d("RULES", "Rules data: " + oneLine);
+                        rule= String.valueOf(doc.getData());
+                        Log.d("RULES", "Rules data: " + rule);
                     }
                     else {
                         Log.d("RULES", "No such document");
@@ -120,48 +110,104 @@ public class UserSettingsRulesActivity extends AppCompatActivity {
                 else {
                     Log.d("RULES", "get failed with ", task.getException());
                 }
-                // todo oneLine data to each data type
-                cpu = oneLine.substring(oneLine.indexOf("CPU=") + 5 , oneLine.length());
+                // todo separate 'rule' variable to each data type variable
+                cpu = rule.substring(rule.indexOf("CPU=") + 5);
                 cpu = cpu.substring(0, cpu.indexOf("}"));
+                cpu_max = cpu.substring(cpu.indexOf("maximum=")+8, cpu.indexOf(","));
+                cpu = cpu.substring(cpu.indexOf("enabled=")+8);
 
-                net_recv = oneLine.substring(oneLine.indexOf("net_recv=") + 10 , oneLine.length());
-                net_recv = net_recv.substring(0, net_recv.indexOf("}"));
+                netR = rule.substring(rule.indexOf("net_recv=") + 10);
+                netR = netR.substring(0, netR.indexOf("}"));
+                net_recv = netR.substring(netR.indexOf("value=")+6, netR.indexOf(","));
+                netR = netR.substring(netR.indexOf("enabled=")+8);
 
-                net_send = oneLine.substring(oneLine.indexOf("net_send=") + 10 , oneLine.length());
-                net_send = net_send.substring(0, net_send.indexOf("}"));
 
-                disk_read = oneLine.substring(oneLine.indexOf("disk_read=") + 11 , oneLine.length());
-                disk_read = disk_read.substring(0, disk_read.indexOf("}"));
+                netS = rule.substring(rule.indexOf("net_send=") + 10);
+                netS = netS.substring(0, netS.indexOf("}"));
+                net_send = netS.substring(netS.indexOf("value=")+6, netS.indexOf(","));
+                netS = netS.substring(netS.indexOf("enabled=")+8);
 
-                disk_write = oneLine.substring(oneLine.indexOf("disk_writ=") + 11 , oneLine.length());
-                disk_write = disk_write.substring(0, disk_write.indexOf("}"));
+                diskR = rule.substring(rule.indexOf("disk_read=") + 11);
+                diskR = diskR.substring(0, diskR.indexOf("}"));
+                disk_read = diskR.substring(diskR.indexOf("value=")+6, diskR.indexOf(","));
+                diskR = diskR.substring(diskR.indexOf("enabled=")+8);
 
-//                System.out.print("\n data value:\n"+cpu + " ~ "+ net_recv+ " ~ "+ net_send + " ~ "+ disk_read + " ~ "+ disk_write + "\n");
 
-                // set each data into relavant textview
-                tv_cpu.setText(cpu);
-                tv_net.setText("net_recv: " + net_recv + "\nnet_send:" + net_send);
-                tv_disk.setText("disk_read: " + disk_read + "\ndisk_write: " +disk_write);
+                diskW = rule.substring(rule.indexOf("disk_writ=") + 11);
+                diskW = diskW.substring(0, diskW.indexOf("}"));
+                disk_write = diskW.substring(diskW.indexOf("value=")+6, diskW.indexOf(","));
+                diskW = diskW.substring(diskW.indexOf("enabled=")+8);
+
+//                System.out.print("\n data value:\n"+cpu_max + " ~ "+ net_recv+ " ~ "+ net_send + " ~ "+ disk_read + " ~ "+ disk_write + "\n");
+
+                // TODO - linear layout for each data type
+                cpu_ll  = (LinearLayout) findViewById(R.id.cpu_layout);
+                net_ll  = (LinearLayout) findViewById(R.id.network_layout);
+                disk_ll = (LinearLayout) findViewById(R.id.disk_layout);
 
                 // todo - call function that create text view dynamically
-                displayRule(cpu);
-                displayRule(net_recv);
-                displayRule(net_send);
-                displayRule(disk_read);
-                displayRule(disk_write);
+                displayRule(cpu_ll, "CPU_max",   cpu_max,    cpu);
+                displayRule(net_ll, "net_send",  net_recv,   netR);
+                displayRule(net_ll, "net_recv",  net_send,   netS);
+                displayRule(disk_ll,"disk_read", disk_read,  diskR);
+                displayRule(disk_ll,"disk_writ", disk_write, diskW);
             }
         });
     }
 
-    public void displayRule(String value){
-        LinearLayout linearLayout = (LinearLayout) findViewById(R.id.ll_example);
+    public void displayRule(LinearLayout ll, String title,  String value, String enabled){
+        LinearLayout newLL = new LinearLayout(this);
+        newLL.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT));
+        newLL.setOrientation(LinearLayout.HORIZONTAL);
+        newLL.setTextAlignment(View.TEXT_ALIGNMENT_TEXT_END);
 
+        LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(0, 100);
+        lp.weight =1;
+
+        // Text view
         TextView textView1 = new TextView(this);
-        textView1.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT,
-                LinearLayout.LayoutParams.WRAP_CONTENT));
-        textView1.setText(value);
-        textView1.setBackgroundColor(0xff66ff66); // hex color 0xAARRGGBB
-        textView1.setPadding(20, 20, 20, 20);// in pixels (left, top, right, bottom)
-        linearLayout.addView(textView1);
+        textView1.setLayoutParams(lp);
+
+        textView1.setText(title +" = "+ value);
+        textView1.setPadding(10, 20, 10, 20);// in pixels (left, top, right, bottom)
+        textView1.setTextSize(20);
+        textView1.setTextColor(Color.BLACK);
+
+        // Check Box
+        CheckBox checkBox = new CheckBox(this);
+//        checkBox.setLayoutParams(lp);
+
+        checkBox.setText("enabled");
+        checkBox.setTextColor(Color.BLACK);
+        checkBox.setButtonTintList(ColorStateList.valueOf(Color.parseColor("#009900")));
+        checkBox.setPadding(10, 10, 30, 30);// in pixels (left, top, right, bottom)
+        checkBox.setChecked(true);
+        checkBox.setEnabled(false);
+
+        // if the data is disabled
+        if(enabled.trim().contains("false"))
+        {
+            textView1.setTextColor(Color.GRAY);
+            checkBox.setText("disabled");
+            checkBox.setChecked(true);
+            checkBox.setButtonTintList(ColorStateList.valueOf(Color.parseColor("#9d7b7b")));
+            checkBox.setTextColor(Color.GRAY);
+            ll.setBackgroundColor(Color.parseColor("#cdcdcd"));
+        }
+        ll.addView(newLL);
+        newLL.addView(textView1);
+        newLL.addView(checkBox);
+
+    }
+
+    // MJ - enable back button in ActionBar
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item){
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                this.finish();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }

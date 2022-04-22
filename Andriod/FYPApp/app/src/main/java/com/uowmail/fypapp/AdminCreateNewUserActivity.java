52812c4f90293e7b431f542acf5c9675ad108f1a
pohshania <pohshania@gmail.com>
2022-04-22 17:09:25 +0800

    added authentication to create new users

--- Andriod/FYPApp/app/src/main/java/com/uowmail/fypapp/AdminCreateNewUserActivity.java
+++ Andriod/FYPApp/app/src/main/java/com/uowmail/fypapp/AdminCreateNewUserActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.util.Log;
@@ -39,6 +40,12 @@ public class AdminCreateNewUserActivity extends AppCompatActivity {
 
     FirebaseAuth fAuth;
     FirebaseFirestore fStore;
+
+    private AlertDialog.Builder dialogBuilder;
+    private AlertDialog dialog;
+    private Button saveChange, cancelBtn;
+    private EditText userEmail, adminPassword;
+    private String email, currAdminEmail, currAdminPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -90,111 +97,118 @@ public class AdminCreateNewUserActivity extends AppCompatActivity {
                     return;
                 }
 
-                // check if the user is already in the system
-                DocumentReference docIdRef = fStore.collection("Users Profile").document(email);
-                docIdRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
-                    @Override
-                    public void onComplete(@NonNull Task<DocumentSnapshot> task) {
-                        if (task.isSuccessful()) {
-                            DocumentSnapshot document = task.getResult();
-                            if (document.exists()) {
-                                //Log.d("FIREBASE QUERY => ", userEmail + " User exists!");
-                                Toast.makeText(AdminCreateNewUserActivity.this, email + " This user exists!", Toast.LENGTH_SHORT).show();
-                                return;
-                            } else {
-                                Log.d("FIREBASE QUERY => ", "User does not exist! Can create this new user!");
-                                toggleKeyboardAndProgressBar(false, true);
+                createNewContactDialog(email, password);
+            }
+        });
+
+    }
+
+    public void createNewContactDialog(String email, String password){
+        dialogBuilder = new AlertDialog.Builder(this);
+        final View passwordPopupView = getLayoutInflater().inflate(R.layout.popup_for_password, null);
+
+        saveChange = (Button) passwordPopupView.findViewById(R.id.saveButton);
+        cancelBtn = (Button) passwordPopupView.findViewById(R.id.cancelButton);
+
+        dialogBuilder.setView(passwordPopupView);
+        dialog = dialogBuilder.create();
+        dialog.show();
 
 
-                                // register the user into Firebase
-                                fAuth.createUserWithEmailAndPassword(email, password).addOnSuccessListener(new OnSuccessListener<AuthResult>() {
-                                    @Override
-                                    public void onSuccess(AuthResult authResult) {
-                                        Toast.makeText(AdminCreateNewUserActivity.this, "User created successfully!", Toast.LENGTH_SHORT).show();
+        cancelBtn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                // define save button here!
+                dialog.dismiss();
+            }
+        });
 
-                                        // getting info of the user that was just created
-                                        FirebaseUser user = fAuth.getCurrentUser();
+        // authentication
+        saveChange.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
 
-                                        CollectionReference usersProfile = fStore.collection("Users Profile");
+                currAdminEmail = fAuth.getCurrentUser().getEmail();
 
-                                        Map<String, Object> data = new HashMap<>();
-                                        data.put("Full Name", createUserFullName.getText().toString().trim());
-                                        data.put("Email", createUserEmail.getText().toString().trim());
-                                        data.put("Organisation ID", createUserOrgID.getText().toString().trim());
-                                        data.put("isAdmin", false);
-                                        data.put("isActive", true);
-                                        data.put("Phone Number", Arrays.asList("000", "001"));
-                                        usersProfile.document(user.getEmail()).set(data);
+                adminPassword = (EditText) dialog.findViewById(R.id.admin_password);
+                currAdminPassword = adminPassword.getText().toString().trim();
 
-                                        createUserProgressBar.setVisibility(View.INVISIBLE);
-                                    }
-                                }).addOnFailureListener(new OnFailureListener() {
-                                    @Override
-                                    public void onFailure(@NonNull Exception e) {
-                                        Toast.makeText(AdminCreateNewUserActivity.this, "Not successful!" + e.toString(), Toast.LENGTH_SHORT).show();
-                                        toggleKeyboardAndProgressBar(true, false);
-                                    }
-                                });
-                            }
-                        } else {
-                            Log.d("FIREBASE QUERY => ", "Failed with: ", task.getException());
-                        }
-                    }
-                });
+                if(TextUtils.isEmpty(currAdminPassword)){
+                    adminPassword.setError("Password is required!");
+                    return;
+                }
 
+                Log.d("ADMIN INFO", currAdminEmail + currAdminPassword);
 
-
-
-     /*           toggleKeyboardAndProgressBar(false, true);
-
-
-                // register the user into Firebase
-                fAuth.createUserWithEmailAndPassword(email, password).addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                fAuth.signInWithEmailAndPassword(currAdminEmail, currAdminPassword).addOnSuccessListener(new OnSuccessListener<AuthResult>() {
                     @Override
                     public void onSuccess(AuthResult authResult) {
-                        Toast.makeText(AdminCreateNewUserActivity.this, "User created successfully!", Toast.LENGTH_SHORT).show();
-
-                        // getting info of the user that was just created
-                        FirebaseUser user = fAuth.getCurrentUser();
-
-                        *//*
-                        DocumentReference df = fStore
-                                .collection("Users").document(user.getEmail())
-                                .collection("Profile").document("Details");
-                        Map<String,Object> userInfo = new HashMap<>();
-                        userInfo.put("Full Name", createUserFullName.getText().toString().trim());
-                        userInfo.put("Email", createUserEmail.getText().toString().trim());
-                        userInfo.put("Organisation ID", createUserOrgID.getText().toString().trim());
-                        // specify user access level
-                        userInfo.put("isAdmin",false);
-
-                        // store the user info into firestore
-                        df.set(userInfo);
-                        *//*
-
-                        CollectionReference usersProfile = fStore.collection("Users Profile");
-
-                        Map<String, Object> data = new HashMap<>();
-                        data.put("Full Name", createUserFullName.getText().toString().trim());
-                        data.put("Email", createUserEmail.getText().toString().trim());
-                        data.put("Organisation ID", createUserOrgID.getText().toString().trim());
-                        data.put("isAdmin", false);
-                        data.put("isActive", true);
-                        data.put("Phone Number", Arrays.asList("000", "001"));
-                        usersProfile.document(user.getEmail()).set(data);
-
-                        createUserProgressBar.setVisibility(View.INVISIBLE);
+                        //Toast.makeText(AdminCreateNewUserActivity.this, "Correct Admin password! Rules are updated.", Toast.LENGTH_SHORT).show();
+                        createUserInFirebase(email, password);
+                        dialog.dismiss();
                     }
                 }).addOnFailureListener(new OnFailureListener() {
                     @Override
                     public void onFailure(@NonNull Exception e) {
-                        Toast.makeText(AdminCreateNewUserActivity.this, "Not successful!" + e.toString(), Toast.LENGTH_SHORT).show();
-                        toggleKeyboardAndProgressBar(true, false);
+                        Toast.makeText(AdminCreateNewUserActivity.this, "Incorrect Admin password." + e.toString(), Toast.LENGTH_SHORT).show();
                     }
-                });*/
+                });
             }
         });
+    }
 
+    private void createUserInFirebase(String email, String password){
+        // check if the user is already in the system
+        DocumentReference docIdRef = fStore.collection("Users Profile").document(email);
+        docIdRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                if (task.isSuccessful()) {
+                    DocumentSnapshot document = task.getResult();
+                    if (document.exists()) {
+                        //Log.d("FIREBASE QUERY => ", userEmail + " User exists!");
+                        Toast.makeText(AdminCreateNewUserActivity.this, email + " This user exists!", Toast.LENGTH_SHORT).show();
+                        return;
+                    } else {
+                        Log.d("FIREBASE QUERY => ", "User does not exist! Can create this new user!");
+                        //toggleKeyboardAndProgressBar(false, true);
+
+
+                        // register the user into Firebase
+                        fAuth.createUserWithEmailAndPassword(email, password).addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                            @Override
+                            public void onSuccess(AuthResult authResult) {
+                                Toast.makeText(AdminCreateNewUserActivity.this, "User created successfully!", Toast.LENGTH_SHORT).show();
+
+                                // getting info of the user that was just created
+                                FirebaseUser user = fAuth.getCurrentUser();
+
+                                CollectionReference usersProfile = fStore.collection("Users Profile");
+
+                                Map<String, Object> data = new HashMap<>();
+                                data.put("Full Name", createUserFullName.getText().toString().trim());
+                                data.put("Email", createUserEmail.getText().toString().trim());
+                                data.put("Organisation ID", createUserOrgID.getText().toString().trim());
+                                data.put("isAdmin", false);
+                                data.put("isActive", true);
+                                data.put("Phone Number", Arrays.asList("000", "001"));
+                                usersProfile.document(user.getEmail()).set(data);
+
+                                createUserProgressBar.setVisibility(View.INVISIBLE);
+                            }
+                        }).addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Toast.makeText(AdminCreateNewUserActivity.this, "Not successful!" + e.toString(), Toast.LENGTH_SHORT).show();
+                                //toggleKeyboardAndProgressBar(true, false);
+                            }
+                        });
+                    }
+                } else {
+                    Log.d("FIREBASE QUERY => ", "Failed with: ", task.getException());
+                }
+            }
+        });
     }
 
     private void fStoreQueryIfUserEmailExists(FirebaseFirestore db, String userEmail){
